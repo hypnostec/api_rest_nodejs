eedc14c074a7521990a490b5c6b6604329d67450
rhoyos1985 <ricbaquero@hotmail.com>
2016-02-29 14:57:34 -0500

    Added files via upload
    Primer commit

---
+++ app.js
@@ -0,0 +1,30 @@
+/*===================================================================
+	Importar Archivos necesarios de la aplicación
+  ===================================================================*/
+var express  = require("express"),
+	bodyParser = require("body-parser"),
+	methodOverride = require("method-override"),
+	mongoose = require("mongoose");
+
+var	app = express(),
+	router = express.Router();
+
+//=====================================================================
+
+/*===================================================================
+	Agregar los midedleware a utilizar en la aplicación
+  ===================================================================*/
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(methodOverride());
+//=====================================================================
+
+router.get('/', function(req, res){
+	res.send("Hello World");
+});
+
+app.use(router);
+
+app.listen(3000, function(){
+	console.log("Servidor corriendo en http://localhost:3000")
+});
